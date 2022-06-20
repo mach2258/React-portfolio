8019db94c71d5fadc7302e932353fea04bf51859
Erich Elste <erichelste@yahoo.com>
2022-06-20 09:20:49 -0400

    finished home page and added project images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import EmailIcon from '@material-ui/icons/Email'
+import "../styles/Home.css"
+
 
 function Home() {
-    return <div>Home</div>
-    
+    return (
+    <div className='home'>
+        <div className='about'>
+            <h2>Hey, My Name is Erich</h2>
+            <div className='prompt'>
+                <p>
+                    A full stack web developer.
+                </p>
+                <LinkedInIcon />
+                <GitHubIcon />
+                <EmailIcon />
+            </div>
+        </div>
+        <div className='skills'> 
+        <h1> Skills </h1>
+        <ol className='list'>
+            <li className='item'>
+                <h2> Front-End</h2>
+                <span> HTML, BootStrap, MaterialUI, NPM, CSS,</span>
+            </li>
+            <li className='item'>
+                <h2> Back-End</h2>
+                <span> NodeJS, ExpressJS, MySQL, ApolloServer </span>
+            </li>
+            <li className='item'>
+                <h2> Languages</h2>
+                <span>JavaScript, Java, React </span>
+            </li>
+        </ol>
+        </div>
+    </div>
+    );
 }
 
 export default Home;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Projectcard from '../components/Projectcard';
+import ProjectOne from "../assets/mach2258.github.io_Weather-music_no playlists.png"
+import ProjectTwo from "../assets/UfoTracker.JPG"
 
 function Projects() {
-    return <div>Projects</div>
+    return <div className='projects'>
+        <h1> My Projects</h1>
+        <div className='projectList'>
+            <Projectcard name="SkyNotes" image={ProjectOne}/>
+            <Projectcard name="SkyNotes" image={ProjectTwo}/>
+        </div>
+    </div>
     
 }
 
