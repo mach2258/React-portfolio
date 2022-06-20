3e6bd2d82d5ee5ee35c00e5813db1e2f71e11ea0
Erich Elste <erichelste@yahoo.com>
2022-06-20 09:32:45 -0400

    project cards given images

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Projectcard from '../components/Projectcard';
 import ProjectOne from "../assets/mach2258.github.io_Weather-music_no playlists.png"
 import ProjectTwo from "../assets/UfoTracker.JPG"
+import ProjectThree from "../assets/Codle.JPG"
+import ProjectFour from "../assets/dj_sona_wallpaper__ethereal__kinetic__concussive__by_rjgtav_d946zy2-pre.png"
+
+import "../styles/Project.css"
 
 function Projects() {
     return <div className='projects'>
         <h1> My Projects</h1>
         <div className='projectList'>
             <Projectcard name="SkyNotes" image={ProjectOne}/>
-            <Projectcard name="SkyNotes" image={ProjectTwo}/>
+            <Projectcard name="UfoTracker" image={ProjectTwo}/>
+            <Projectcard name="Codle" image={ProjectThree}/>
+            <Projectcard name="DJ Sona Discord Bot" image={ProjectFour}/>
         </div>
     </div>
     
